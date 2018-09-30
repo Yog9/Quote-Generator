f99506e6f57ccad5a10632b8ab781e7466298737
yogita <veryogita@gmail.com>
2018-09-30 19:27:16 +0530

    Files added

---
+++ js/Quotes.js
@@ -0,0 +1,67 @@
+//Quotes.js
+var quotes = [
+  {//quote 0
+	id:1,
+    quote : "Wilderness is not a luxury but necessity of the human spirit.",
+    source : "Edward, ",
+    citation : "Desert ,",
+    year : 1968,
+    tags : ["inspirational", "nature", "wilderness"]
+  },
+  {//quote 1
+    id:2,
+    quote : "We do not see nature with our eyes, but with our understandings and our hearts.",
+    source : "William Hazlett",
+	citation:"",
+    year : 1818,
+    tags : ["inspirational", "nature"]
+  },
+  {//quote 2
+	id:3,
+    quote : "A foot and lighthearted I take to the open road, healthy, free, the world before me. Henceforth, I ask not good fortune, I myself am good fortune. Henceforth, I whimper no more, postpone no more, need nothing.",
+    source : "Walt Whitman",
+    citation : ",Songs for the Open Road",
+	year:"",
+    tags : ["inspirational", "nature", "adventure", "wise"]
+  },
+  {//quote 3
+	id:4,
+    quote : 'I felt my lungs inflate with the onrush of scenery - air, mountains, trees, people. I thought, "this is what it is to be happy."',
+    source : "Sylvia Plath",
+    citation : ",The Bell Jar,",
+    year : 1963,
+    tags : ["nature", "outside", "inspirational"]
+  },
+  {//quote 4
+	id:5,
+    quote : "Not just beautiful, though - the stars are like the trees in the forest, alive and breathing. And they're watching me.",
+    source : "Haruki Murakami",
+    citation : ",Kafka on the Shore,",
+    year : 2002,
+    tags : ["beautiful", "forest", "thoughful", "stars", "nature"]
+  },
+  {//quote 5
+	id:6,
+    quote : "If we Surrendered to earth's intelligence we could rise up rooted, like trees.",
+    source : "Rainer Maria Rilke",
+    citation : ",Rilke's Book of Hours,",
+    year : 1905,
+    tags : ["thoughful", "stars", "nature"]
+  },
+  {//quote 6
+	id:7,
+    quote : "Heaven is under our feet as well as over our heads.",
+    source : "Henery David Thoreau",
+    citation : ",Walden,",
+    year : 1854,
+    tags : ["wise", "nature", "thoughful"]
+  },
+  {//quote 7
+	id:8,
+    quote : "I wonder if the snow loves the trees and fields, that it kisses them so gently? And then it covers them up snug, you know, with a white quilt; and perphaps it says 'Go to sleep, darlings, till the summer comes again.'",
+    source : "Lewis Carroll",
+    citation : ",Alice's Adventures in Wonderland & Through the Looking Glass,",
+    year : 1865,
+    tags : ["beautiful", "forest", "thoughful", "nature", "loving"]
+  }
+];
